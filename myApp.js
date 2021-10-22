9163c40cebd10966ad7ce0f45e4901f09e6f29da
Glitch (gregarious-bony-gourd) <none>
2021-10-22 04:12:51 +0000

    👐🍥 Checkpoint ./myApp.js:37756627/181

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
-require('mongoose')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
 
 
 let Person;
