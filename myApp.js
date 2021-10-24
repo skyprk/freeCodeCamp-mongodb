f7c63826ba565e7e681acef2ca720e2161808fb6
Glitch (gregarious-bony-gourd) <none>
2021-10-24 06:11:52 +0000

    🔨🌪 Checkpoint ./myApp.js:37756627/217

--- myApp.js
+++ myApp.js
@@ -49,7 +49,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+    Person.findById(personId, (err, data) => {
+        if (err) return console.error(err)
+        done(null, data)
+    })
 };
 
 const findEditThenSave = (personId, done) => {
