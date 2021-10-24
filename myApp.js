06045486d464a5b923113062c6236c03a78ce0d6
Glitch (gregarious-bony-gourd) <none>
2021-10-24 06:49:24 +0000

    Updating a record using findOneAndUpdate method

--- myApp.js
+++ myApp.js
@@ -73,15 +73,10 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  Person.findOneAndUpdate({name: personName},
-    {age: ageToSet},
-    {new: true},
-    (err, data) => {
-      if (err) return console.error(err)
-      
-      done(null, data)
-    }
-  )
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, data) => {
+    if (err) return console.error(err)
+    done(null, data)
+  })
 };
 
 const removeById = (personId, done) => {
